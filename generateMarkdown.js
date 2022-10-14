adef2fd2b3ff6367ff250e8122028d91c12f9062
dankturtle <abrowncownow@gmail.com>
2022-10-13 22:52:21 -0700

    decided to go with es6 node. Pimped out my code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -24,4 +24,4 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+export default generateMarkdown;
--- index.js
+++ index.js
@@ -1,13 +1,52 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const { generate } = require('rxjs');
-const generateMarkdown = require('./generateMarkdown')
+#!/usr/bin/env node
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+import fs from 'fs';
+import generateMarkdown from './generateMarkdown.js';
+
 var data;
 var readMe;
-
+var name;
 
 //Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 //Declare Functions
+
+const sleep = (ms = 2000) => new Promise ((r)=> setTimeout(r,ms))
+
+async function askName(){
+    const answers = await inquirer.prompt({
+        name: 'name',
+        type: 'input',
+        message: 'What is your name?',
+        default() {
+            return 'Steve';
+        },
+    })
+    name = answers.name;
+}
+
+async function welcome() {
+    const rainbowWelcome = chalkAnimation.rainbow(
+        `${name}, Welcome to the ReadMe Gen Pro 9000! \n`
+    );
+
+    await sleep();
+    rainbowWelcome.stop();
+
+    console.log(`
+        ${chalk.bgBlue('How to use this tool:')}
+        Answer the questions regarding your project.
+        I will digest all of your weak answers.
+        I will then poop out a beautiful readeMe.md file for you to plagiarize.
+    `)
+}
+
+
+
 function getData() {
     inquirer
         .prompt([
@@ -51,22 +90,35 @@ function getData() {
         .then((answers) => {
             data = answers;
             console.log(data);
-            readMe = generateMarkdown.generateMarkdown(data);
+            handleAnswer();
         });
 }
 
-function updateReadMe(data){
-    for (const answer of data){
-
-    }
+async function handleAnswer(){
+    const spinner = createSpinner('Beep. Boop.. Processing...').start();
+    await sleep();
+    readMe = generateMarkdown(data);
+    spinner.success({ text: `Good job me!`});
+    await sleep();
+    await winner();
 }
-
+async function winner(){
+    console.clear();
+    const msg = `Congrats, ${name}!\nYou made a Pro ReadMe!`;
+    figlet(msg, (err,data) => {
+        console.log(gradient.pastel.multiline(data));
+    });
+    await sleep();
+    console.log(readMe);
+}
 function init(){
-    getData();
+      
 }
 //declare functions
 
 //Readme 
 
 //init
-init();
+await askName();
+await welcome();
+await getData();
